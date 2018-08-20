9c8a0e564f6c1e0cf2917604649af2869094fe97
Ben Davies <ben.davies@uwaterloo.ca>
2018-08-20 14:11:42 -0400

    Added segment generation of api.h from params.

--- oqs/generate.py
+++ oqs/generate.py
@@ -18,8 +18,34 @@ def parse_api_header(content):
         for kw in KEYWORDS:
             if kw in line:
                 _, _, value = line.strip().split()
-                result[kw] = value.strip()
+                value = value.strip()
+
+                if kw == CRYPTO_ALGNAME:
+                    assert value[0] == value[-1] == '"'
+                    value = value[1:-1]
+
+                result[kw] = value
                 break
 
     return result
 
+
+API_HEADER_SEGMENT_TEMPLATE = """#ifdef OQS_ENABLE_KEM_{CRYPTO_ALGNAME}
+
+#define OQS_KEM_{CRYPTO_ALGNAME}_length_public_key {CRYPTO_PUBLICKEYBYTES}
+#define OQS_KEM_{CRYPTO_ALGNAME}_length_secret_key {CRYPTO_SECRETKEYBYTES}
+#define OQS_KEM_{CRYPTO_ALGNAME}_length_ciphertext {CRYPTO_CIPHERTEXTBYTES}
+#define OQS_KEM_{CRYPTO_ALGNAME}_length_shared_secret {CRYPTO_BYTES}
+
+OQS_KEM *OQS_KEM_{CRYPTO_ALGNAME}_new();
+
+extern OQS_STATUS OQS_KEM_{CRYPTO_ALGNAME}_keypair(uint8_t *public_key, uint8_t *secret_key);
+extern OQS_STATUS OQS_KEM_{CRYPTO_ALGNAME}_encaps(uint8_t *ciphertext, uint8_t *shared_secret, const uint8_t *public_key);
+extern OQS_STATUS OQS_KEM_{CRYPTO_ALGNAME}_decaps(uint8_t *shared_secret, const unsigned char *ciphertext, const uint8_t *secret_key);
+
+#endif
+"""
+
+
+def kem_header_segment(params):
+    return API_HEADER_SEGMENT_TEMPLATE.format_map(params)
--- tests/test_generate.py
+++ tests/test_generate.py
@@ -17,6 +17,24 @@ int crypto_kem_dec(unsigned char *ss, const unsigned char *ct, const unsigned ch
 """
 
 
+EXP_KEM_HEADER_SEGMENT = """
+#ifdef OQS_ENABLE_KEM_FakeAlg1
+
+#define OQS_KEM_FakeAlg1_length_public_key 10
+#define OQS_KEM_FakeAlg1_length_secret_key 15
+#define OQS_KEM_FakeAlg1_length_ciphertext 20
+#define OQS_KEM_FakeAlg1_length_shared_secret 25
+
+OQS_KEM *OQS_KEM_FakeAlg1_new();
+
+extern OQS_STATUS OQS_KEM_FakeAlg1_keypair(uint8_t *public_key, uint8_t *secret_key);
+extern OQS_STATUS OQS_KEM_FakeAlg1_encaps(uint8_t *ciphertext, uint8_t *shared_secret, const uint8_t *public_key);
+extern OQS_STATUS OQS_KEM_FakeAlg1_decaps(uint8_t *shared_secret, const unsigned char *ciphertext, const uint8_t *secret_key);
+
+#endif
+"""
+
+
 def test_parse_api_header():
     res = generate.parse_api_header(FAKE_API_HEADER)
     assert len(res) == 5
@@ -24,4 +42,21 @@ def test_parse_api_header():
     assert res['CRYPTO_PUBLICKEYBYTES'] == '2'
     assert res['CRYPTO_BYTES'] == '3'
     assert res['CRYPTO_CIPHERTEXTBYTES'] == '4'
-    assert res['CRYPTO_ALGNAME'] == '"NAME"'
+    assert res['CRYPTO_ALGNAME'] == 'NAME'
+
+
+def get_fake_api_params(pk='10', sk='15', ct='20', ss='25', name='FakeAlg1'):
+    params = {'CRYPTO_PUBLICKEYBYTES': pk,
+              'CRYPTO_SECRETKEYBYTES': sk,
+              'CRYPTO_CIPHERTEXTBYTES': ct,
+              'CRYPTO_BYTES': ss,  # shared secret
+              'CRYPTO_ALGNAME': name}
+
+    return params
+
+
+# TODO: handle spaces in names
+def test_kem_header_segment():
+    api_params = get_fake_api_params()
+    header = generate.kem_header_segment(api_params)
+    assert header in EXP_KEM_HEADER_SEGMENT  # allow some whitespace leeway
